f3bf2ce1bc1cb604ebb397a841451c9f1289944c
rootshellace <rootshellace@localhost.localdomain>
2022-08-10 01:20:27 +0300

    Updated design with color schema and others

--- PassWordGenerator.py
+++ PassWordGenerator.py
@@ -7,11 +7,11 @@ import string
 import random
 
 main_program = tkinter.Tk()
-main_program.title('Password Generator')
+main_program.title('Password Generator  ©rootshellace')
 main_program.resizable(0, 0)
 main_program.geometry('640x360')
 
-main_program.configure(bg='gray')
+main_program.configure(bg='#006400')
 
 main_program.columnconfigure(index=0, weight=1)
 main_program.columnconfigure(index=1, weight=1)
@@ -141,23 +141,32 @@ def generate_password():
 		generated_password_entry.insert(0, final_password)
 		generated_password_entry.configure(state='readonly')
 
+
+checkbox_style = tkinter.ttk.Style()
+checkbox_style.configure('PwChar.TCheckbutton', background='#006400', foreground='#FBB117', font=('Calibri', 10))
+checkbox_style.map('PwChar.TCheckbutton', background=[('active', '#006400')], font=[('selected', ('Calibri', 10, 'italic'))])
+
+radiobutton_style = tkinter.ttk.Style()
+radiobutton_style.configure('PwLen.TRadiobutton', background='#006400', foreground='#FBB117', font=('Calibri', 10))
+radiobutton_style.map('PwLen.TRadiobutton', background=[('active', '#006400')], font=[('selected', ('Calibri', 10, 'italic'))])
+
 window_content = tkinter.ttk.Frame(main_program).grid(row=0, column=0)
 
 test_label = tkinter.ttk.Label(window_content, text='', font=('Calibri', 14), justify=tkinter.CENTER,
-								background='gray')
+								background='#006400')
 test_label.grid(row=0, column=0, columnspan=5)
 
-welcome_label = tkinter.ttk.Label(window_content, text=' Welcome! ', font=('Calibri', 14), justify=tkinter.CENTER,
-									background='gray', foreground='black', relief='flat')
+welcome_label = tkinter.ttk.Label(window_content, text=' Welcome! ', font=('Calibri', 16, 'bold'), justify=tkinter.CENTER,
+									background='#006400', foreground='black', relief='flat')
 welcome_label.grid(row=1, column=0, columnspan=5)
 
-main_msg_label = tkinter.ttk.Label(window_content, text=' Use this tool to generate a random password! ', font=('Calibri', 12), 
-								justify=tkinter.CENTER, background='gray', foreground='black', relief='flat')
+main_msg_label = tkinter.ttk.Label(window_content, text=' Use this tool to generate a random password! ', font=('Calibri', 14, 'bold'), 
+								justify=tkinter.CENTER, background='#006400', foreground='black', relief='flat')
 main_msg_label.grid(row=2, column=0, columnspan=5, pady=10)
 
 
-chars_used_label = tkinter.ttk.Label(window_content, text='Password Chars:', font=('Calibri', 10),
-										justify=tkinter.CENTER, background='red', foreground='black')
+chars_used_label = tkinter.ttk.Label(window_content, text='Password Chars:', font=('Calibri', 10, 'bold'),
+										justify=tkinter.CENTER, background='#006400', foreground='#FBB117')
 chars_used_label.grid(row=3, column=0, padx=5, pady=20, ipadx=2, ipady=2, sticky=tkinter.E)
 
 upper_char_val = tkinter.IntVar()
@@ -166,27 +175,36 @@ lower_char_val.set(1)
 numeric_char_val = tkinter.IntVar()
 symbol_char_val = tkinter.IntVar()
 
-upper_char_checkbox = tkinter.ttk.Checkbutton(window_content, text='Use uppercase', onvalue=1, offvalue=0, variable=upper_char_val)
-lower_char_checkbox = tkinter.ttk.Checkbutton(window_content, text='Use lowercase', onvalue=1, offvalue=0, variable=lower_char_val)
-numeric_char_checkbox = tkinter.ttk.Checkbutton(window_content, text='Use numbers', onvalue=1, offvalue=0, variable=numeric_char_val)
-symbol_char_checkbox = tkinter.ttk.Checkbutton(window_content, text='Use symbols', onvalue=1, offvalue=0, variable=symbol_char_val)
+
+
+upper_char_checkbox = tkinter.ttk.Checkbutton(window_content, text='Use uppercase', style='PwChar.TCheckbutton', 
+												onvalue=1, offvalue=0, variable=upper_char_val)
+lower_char_checkbox = tkinter.ttk.Checkbutton(window_content, text='Use lowercase', style='PwChar.TCheckbutton',
+												onvalue=1, offvalue=0, variable=lower_char_val)
+numeric_char_checkbox = tkinter.ttk.Checkbutton(window_content, text='Use numbers', style='PwChar.TCheckbutton',
+												onvalue=1, offvalue=0, variable=numeric_char_val)
+symbol_char_checkbox = tkinter.ttk.Checkbutton(window_content, text='Use symbols', style='PwChar.TCheckbutton',
+												onvalue=1, offvalue=0, variable=symbol_char_val)
 
 upper_char_checkbox.grid(row=3, column=1, padx=2, ipadx=2, ipady=2, sticky=tkinter.W)
 lower_char_checkbox.grid(row=3, column=2, padx=2, ipadx=2, ipady=2, sticky=tkinter.W)
 numeric_char_checkbox.grid(row=3, column=3, padx=2, ipadx=2, ipady=2, sticky=tkinter.W)
 symbol_char_checkbox.grid(row=3, column=4, padx=2, ipadx=10, ipady=2, sticky=tkinter.W)
 
-pass_length_label = tkinter.ttk.Label(window_content, text='Length -> 4 - 50:', font=('Calibri', 10),
-										background='red', foreground='black')
+pass_length_label = tkinter.ttk.Label(window_content, text='Length -> 4 - 50:', font=('Calibri', 10, 'bold'),
+										background='#006400', foreground='#FBB117')
 pass_length_label.grid(row=4, column=0, ipadx=2, padx=5, sticky=tkinter.E)
 
 selected_pass_length = tkinter.IntVar()
 
 selected_pass_length.set(1)
 
-pass_small_option = tkinter.ttk.Radiobutton(window_content, text='8-12 chars', value=1, variable=selected_pass_length, command=deactivate_custom_entry)
-pass_medium_option = tkinter.ttk.Radiobutton(window_content, text='13-16 chars', value=2, variable=selected_pass_length, command=deactivate_custom_entry)
-pass_custom_option = tkinter.ttk.Radiobutton(window_content, text='Custom no. :', value=3, variable=selected_pass_length, command=activate_custom_entry)
+pass_small_option = tkinter.ttk.Radiobutton(window_content, text='8-12 chars', style='PwLen.TRadiobutton', value=1, 
+											variable=selected_pass_length, command=deactivate_custom_entry)
+pass_medium_option = tkinter.ttk.Radiobutton(window_content, text='13-16 chars', style='PwLen.TRadiobutton', value=2, 
+											variable=selected_pass_length, command=deactivate_custom_entry)
+pass_custom_option = tkinter.ttk.Radiobutton(window_content, text='Custom no. :', style='PwLen.TRadiobutton', value=3, 
+											variable=selected_pass_length, command=activate_custom_entry)
 
 pass_small_option.grid(row=4, column=1, sticky=tkinter.W, padx=2, ipadx=2, ipady=2)
 pass_medium_option.grid(row=4, column=2, sticky=tkinter.W, padx=2, ipadx=2, ipady=2)
